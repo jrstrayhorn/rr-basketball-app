91486c5705414a76961864eeef6fa1dc208d729e
stepforwardtech <jrstrayhorn@gmail.com>
2018-02-28 14:37:41 -0500

    Finished fetching the players

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,8 +1,47 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import { getPlayers } from '../api';
+import { parse } from 'query-string';
+import slug from 'slug';
 
 class Players extends Component {
+  state = {
+    players: [],
+    loading: true
+  };
+  componentDidMount() {
+    const { location } = this.props;
+
+    location.search
+      ? this.fetchPlayers(parse(location.search).teamId)
+      : this.fetchPlayers();
+  }
+  fetchPlayers = async teamId => {
+    const players = await getPlayers(teamId);
+    this.setState(() => ({
+      loading: false,
+      players
+    }));
+  };
   render() {
-    return <div className="container two-column">PLAYERS</div>;
+    const { players, loading } = this.state;
+    const { match, location } = this.props;
+    return (
+      <div className="container two-column">
+        <Sidebar
+          loading={loading}
+          title="Players"
+          list={players.map(player => player.name)}
+          {...this.props}
+        />
+
+        {loading === false &&
+          location.pathname === '/players' && (
+            <div className="sidebar-instruction">Select a Player</div>
+          )}
+      </div>
+    );
   }
 }
 
