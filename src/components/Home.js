9aa91044cf180822add36f83660fb23d55332340
stepforwardtech <jrstrayhorn@gmail.com>
2018-02-28 14:02:35 -0500

    Finished setting up the Home Route

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
+import TeamLogo from './TeamLogo';
+import { Link } from 'react-router-dom';
+import { getTeamNames } from '../api';
 
 class Home extends Component {
+  state = {
+    teamNames: []
+  };
+  async componentDidMount() {
+    const teamNames = await getTeamNames();
+    this.setState(() => ({ teamNames }));
+  }
   render() {
-    return <div className="container">Home</div>;
+    const { teamNames } = this.state;
+    return (
+      <div className="container">
+        <h1 className="large-header">RR Basketball League</h1>
+        <h3 className="header text-center">Select a team</h3>
+        <div className="home-grid">
+          {teamNames.map(id => (
+            <Link key={id} to={`/${id}`}>
+              <TeamLogo id={id} width="125px" />
+            </Link>
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 
